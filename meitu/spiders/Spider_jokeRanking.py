a51dad54e6fa29526587611d075644710f70f936
gryphon <2314668686@qq.com>
2015-12-06 22:11:52 +0800

    add jokeRanking

--- meitu/spiders/Spider_jokeRanking.py
+++ meitu/spiders/Spider_jokeRanking.py
@@ -1,34 +1,19 @@
 # -*- coding: utf-8 -*-
-
 import scrapy
 import re
 
 from meitu.items import Joke
 
-#笑话分类
-class Spider_jokeType(scrapy.Spider):
-	name = 'jokeType'
+class Spider_jokeRanking(scrapy.Spider):
+	name = 'jokeRanking'
 
-	start_urls =[
-		'http://www.jokeji.cn/Keyword.htm',
+	start_urls = [
+		'http://www.jokeji.cn/hot.htm',
 	]
 
 	baseUrl = 'http://www.jokeji.cn'
 
 	def parse(self, response):
-		links = response.xpath('//a[contains(@href, "/list")]')
-		links.extract()
-
-		for index, link in enumerate(links):
-			args = (index, link.xpath('@href').extract(), link.xpath('text()').extract())
-			type = args[2][0]
-
-			url = self.baseUrl + args[1][0]
-
-			yield scrapy.Request(url,callback=self.handlePages,meta={'type':type})
-
-	def handlePages(self,response):
-		type = response.meta['type']
 		pageCount = 0
 		for pages in response.xpath('//a[contains(@href, "keyword.asp?me_page=")]'):
 			pageName = pages.xpath('./img/@alt').extract()
@@ -51,21 +36,22 @@ class Spider_jokeType(scrapy.Spider):
 		for jokes in response.xpath('//table[contains(@background, "images/")]'):
 			joke = Joke()
 			joke['type'] = type
-			title = jokes.xpath('./tr/td[2]/a[@class="main_14"]/text()').extract()[0]
-			joke['title'] = title
 
-			url = jokes.xpath('./tr/td[2]/a[@class="main_14"]/@href').extract()[0]
-			requestUrl = self.baseUrl + url
+			title = jokes.xpath('./tr/td[2]/a[@class="main_14"]/text()').extract()
+			if title:
+				joke['title'] = title
 
-			yield scrapy.Request(requestUrl,callback=self.handleContent,meta={'joke':joke})
+			url = jokes.xpath('./tr/td[2]/a[@class="main_14"]/@href').extract()
+			if url:
+				url = url[0]
+				requestUrl = self.baseUrl + url
+				yield scrapy.Request(requestUrl,callback=self.handleContent,meta={'joke':joke})
 
 	def handleContent(self,response):
 		joke = response.meta['joke']
-
 		joke['content'] = []
 		for contents in response.xpath('//span[@id="text110"]/p'):
-			content = contents.xpath('./text()').extract()
+			content = contents.xpath('./font/text()').extract()
 			if content:
 				joke['content'].append(content[0])
-
 		return joke
